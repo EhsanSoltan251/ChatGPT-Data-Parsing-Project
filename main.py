5b21dad903b2bd1f6f01a85606d19506913dcd68
EhsanSoltan251 <97134888+EhsanSoltan251@users.noreply.github.com>
2023-06-08 19:40:09 -0600

    .

--- main.py
+++ main.py
@@ -6,20 +6,19 @@ from os import listdir
 from os.path import isfile, join
 
 
-
-
 pdfs_directory = os.path.dirname(os.path.abspath(__file__)) + "/pdfs"
 pdf_paths = [pdfs_directory + "/" + file for file in listdir(pdfs_directory) if  file[-4:] == ".pdf" and isfile(join(pdfs_directory, file))]
 
+#insert your own api key here
 openai.api_key = "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"
 
-prompt = """i want you to output a JSON file. each key will be the name of a specific electronic device
-and, the value of which will be the corresponding effect of radiation on that specific device.
+prompt = """I want you to output a JSON file. each key will be the name of a specific electronic device
+and, the value of which will be the corresponding effect of radiation on that specific device. Thank you.
 
 """
 
 
-'''Converts a pdf into a string. Takes in pdf path as an argument'''
+'''Converts a pdf into a string. Takes in an absolute path as an argument'''
 def pdfToString(path):
         text = ""
 
@@ -31,6 +30,7 @@ def pdfToString(path):
                     
         return text
 
+'''Runs an input through the GPT API. takes an input string as an argument'''
 def gptInput(input):
        gpt = openai.ChatCompletion.create(
               model="gpt-3.5-turbo",
@@ -38,10 +38,14 @@ def gptInput(input):
        )
        return gpt.choices[0].message.content
 
+
+
+
 results = []
+
 for path in pdf_paths:
        paper = pdfToString(path)
-       paper = paper[5500:8500]
+       paper = paper[5500:8500] #taking a subsection of the paper here because of token limit
        full_prompt = prompt + paper
        results.append(gptInput(full_prompt))
        
