e29b113fbe52991df016f6b1e605a87386f56ada
EhsanSoltan251 <97134888+EhsanSoltan251@users.noreply.github.com>
2023-06-22 10:50:32 -0600

    Add remove redundant info

--- main.py
+++ main.py
@@ -11,13 +11,17 @@ from os.path import isfile, join
 pdfs_directory = os.path.dirname(os.path.abspath(__file__)) + "/pdfs"
 pdf_paths = [pdfs_directory + "/" + file for file in listdir(pdfs_directory) if  file[-4:] == ".pdf" and isfile(join(pdfs_directory, file))]
 
-openai.api_key = "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"
+openai.api_key = "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"
 
-prompt = """This paper is about the effects of radiation on different electronic devices
-please output a json file with keys as a specific device and values as the effects
-of radiation for that specific device.
-
-"""
+prompts = [
+       '''What devices were tested in this paper? Please give the items and a summary for each''',
+       '''Can you briefly summarize the single event effect testing that was done and the
+          found results for the device(s) that were tested?''',
+       '''Can you briefly summarize the total ionizing dose testing that was done and the found results
+          for the device(s) that were tested?''',
+       '''Can you briefly summarize any interesting data that was found about the device
+          that was tested in terms of radiation effects?'''
+]
 
 heading_prompt = """if the following passage contains any paragraph headings, give me their names.
 
@@ -44,25 +48,31 @@ def gptInput(input):
 
 results = []
 headings = []
-for path in pdf_paths:
+for path_index, path in enumerate(pdf_paths):
        paper = pdfToString(path)
-       full_prompt = prompt + paper
-       #results.append(gptInput(full_prompt))
 
-
-       step = int(len(paper) / 4)
-       for i in range(4):
-              paper_subsection = paper[i * step : i * step + step]
-              full_heading_prompt = heading_prompt + paper_subsection
-              full_prompt = prompt + paper_subsection
-              #print(paper_subsection)
-              #headings.append(gptInput(full_heading_prompt))
+       results.append([])
+       for prompt in prompts[0:1]:
+              full_prompt = prompt + paper
               #results.append(gptInput(full_prompt))
-       #print(headings)
-       #print(results)
-       print(paper)
+
+
+              step = int(len(paper) / 4)
+              full_reply = ""
+              for i in range(4):
+                     paper_subsection = paper[i * step : i * step + step]
+                     full_prompt = prompt + paper_subsection
+                     
+                     #full_heading_prompt = heading_prompt + paper_subsection
+                     #print(paper_subsection)
+                     #headings.append(gptInput(full_heading_prompt))
+
+                     full_reply += gptInput(full_prompt + " ")
+              remove_redundant = gptInput("Please remove all duplicate information from the following passage: " + full_reply)
+              results[path_index].append(remove_redundant)
        
-#print(results)
+       
+print(results)
 
 
     
